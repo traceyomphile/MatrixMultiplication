09ca12b9a6aaeac36e75e00d8bccc6bf63ada081
traceyomphile <omphiletracey@gmail.com>
2025-11-24 15:04:40 +0200

    Created a MatrixRequest class to help build backend

---
+++ MatrixRequest.java
@@ -0,0 +1,11 @@
+
+public class MatrixRequest {
+    private double[][] matrixA;
+    private double[][] matrixB;
+
+    public double[][] getMatrixA() { return matrixA; }
+    public void setMatrixA(double[][] matrixA) { this.matrixA = matrixA; }
+
+    public double[][] getMatrixB() { return matrixB; }
+    public void setMatrixB(double[][] matrixB) { this.matrixB = matrixB; }
+}
